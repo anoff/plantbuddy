2ecf157571d297daca8bc8b3e60901800f15a094
anoff <offenhaeuser@gmail.com>
2018-08-17 15:06:30 +0200

    :ant: fix: ignore aggregates

--- web/src/components/Chart.vue
+++ web/src/components/Chart.vue
@@ -176,7 +176,7 @@ export default {
       if (this.zoomLevel > 3) aggregate = 'day'
       // hacky workaround for older data
       let response
-      if (new Date(from).toISOString() < '2018-08-07T23:51:40.047Z') {
+      if (false || new Date(from).toISOString() < '2018-08-07T23:51:40.047Z') {
         if (aggregate === 'none') {
           response = db.collection('data')
           .where('date', '<=', until)
